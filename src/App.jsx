5c02f47cbf33df4e02aec1d22e862db2846f4ddc
Ijeona <Ijeona@users.noreply.github.com>
2023-02-14 20:25:08 +0800

    Base set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+
+//pages//
+import HomePage from "./pages/HomePage";
+import ProjectPage from "./pages/ProjectPage";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
+
+//components//
+import Nav from "./components/Nav/Nav";
+
+//CSS//
+import "./App.css";
+
+const HeaderLayout = () => (
+  <div>
+    <Nav />
+    <Outlet />
+  </div>
+);
+
+const router = createBrowserRouter([
+  {
+    element: <HeaderLayout />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/project", element: <ProjectPage/>},
+      { path: "/about", element: <AboutPage /> },
+      { path: "/contact", element: <ContactPage /> },
+    ],
+  },
+]);
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <RouterProvider router={router} />
+  );
 }
 
-export default App
+export default App;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,17 @@
+// Components
+import ProjectCard from "../components/ProjectCard/ProjectCard";
+
+// Data
+import { allProjects } from "../data";
+
 function HomePage() {
-    return <h1>This is the Home Page</h1>;
+  return (
+    <div id="project-list">
+      {allProjects.map((project, key) => {
+        return <ProjectCard key={key} projectData={project} />;
+      })}
+    </div>
+  );
 }
 
 export default HomePage;
