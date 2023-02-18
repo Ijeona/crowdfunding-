78156d7505a6bcb3423c755e0509e4378e926f0d
Ijeona <Ijeona@users.noreply.github.com>
2023-02-18 10:09:17 +0800

    attach card

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -8,7 +8,7 @@ function ProjectCard(props) {
   const { projectData } = props;
 
   return (
-    <div>
+    <div className="project-card">
       <Link to="/project">
         <img src={projectData.image} />
         <h3>{projectData.title}</h3>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,29 @@
+import { useState, useEffect } from "react";
+
 // Components
 import ProjectCard from "../components/ProjectCard/ProjectCard";
 
-// Data
-import { allProjects } from "../data";
-
 function HomePage() {
-  return (
-    <div id="project-list">
-      {allProjects.map((project, key) => {
-        return <ProjectCard key={key} projectData={project} />;
-      })}
-    </div>
-  );
+    //state//
+    const [projectList, setProjectList] = useState([]);
+
+    useEffect(() => {
+        fetch(`${import.meta.env.VITE_API_URL}projects`)
+            .then((results) => {
+                return results.json();
+            })
+            .then((data) => {
+                setProjectList(data);
+            });
+    }, []);
+
+    return (
+        <div id="project-list">
+            {projectList.map((project, key) => {
+                return <ProjectCard key={key} projectData={project} />;
+            })}
+        </div>
+    );
 }
 
 export default HomePage;
